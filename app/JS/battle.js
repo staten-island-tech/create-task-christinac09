6c4ddae76767ce1ae81ad90b9538369b1d5a5734
christina <158211764+christinac09@users.noreply.github.com>
2025-01-03 14:01:46 -0500

    Update battle.js

--- app/JS/battle.js
+++ app/JS/battle.js
@@ -24,9 +24,30 @@ async function getCharacterSkills(character) {
   }
 }
 
+function getRandomAttackValue(attackType, characterSkills) {
+  let damageString;
+  if (attackType === "normal") {
+    damageString = characterSkills[0]["attribute-scaling"][0].value;
+  } else if (attackType === "elemental") {
+    damageString = characterSkills[1]["attribute-scaling"][0].value;
+  } else if (attackType === "elemental burst") {
+    damageString = characterSkills[2]["attribute-scaling"][0].value;
+  } else {
+    alert("somethings wrong w attack type");
+  }
+  const damageList = damageString.split(" ");
+  console.log(damageList);
+  const randomInt = Math.floor(Math.random() * damageList.length);
+  const randomDamage = damageList[randomInt];
+  return randomDamage;
+}
+const skills = await getCharacterSkills("albedo");
+/* console.log(getRandomAttackValue("normal", skills)); */
+
 document.getElementById("normal").addEventListener("click", function () {
   // define damage variable
-  // run damage function w damage parameter
+  const damage = getRandomAttackValue("normal", skills);
+  // run damage function w damage parameter + update health & commentary stuff
 });
 
 export { getCharacterSkills };
